a50344c6ed1e88fe4e2659b2e18d1a3b5f681059
noontarineecpf <49976899+noontarineecpf@users.noreply.github.com>
2019-04-29 10:12:02 +0700

    Update receiveController.js

--- src/router/api/v1/controllers/receiveController.js
+++ src/router/api/v1/controllers/receiveController.js
@@ -22,6 +22,7 @@ const getPanelConfigs = async ctx => {
         //console.log("object");
         const config = await panelConfig.getPanelConfig(ctx.params.panelId);
         ctx.body = JSON.stringify(config);
+        console.log("test");
     } catch (error) {
         console.log(error);
     }
