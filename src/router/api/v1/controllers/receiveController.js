6873aaed3c1a9628646256f3c9aa88458311b694
noontarineecpf <49976899+noontarineecpf@users.noreply.github.com>
2019-05-02 10:00:40 +0700

    Update receiveController.js
    AA

--- src/router/api/v1/controllers/receiveController.js
+++ src/router/api/v1/controllers/receiveController.js
@@ -1,34 +1,66 @@
 const moment = require("moment");
 const constants = require("../../../../utils/constants");
-
 const panelConfig = require("../panelConfig");
 const processDate = require("../processDate");
 const productionOrders = require("../productionOrder");
+
+
 const getProductionOrders = async ctx => {
     try {
-        const config = await panelConfig.getPanelConfig(ctx.params.panelId);
-        //const locationCode = config.find(o => o.PANEL_KEY === 'LocationCode').PANEL_VALUE;
-        const locationCode = config.LocationCode;
+        console.log(ctx.params.panelId);
+        const [config] = await panelConfig.getPanelConfig(ctx.params.panelId);
+        console.log(config.LOCATIONCODE);
+        const locationCode = config.LOCATIONCODE;
+        console.log(locationCode);
         const productionDate = await processDate.getProcessDate(ctx.params.plantCode, locationCode);
         const resultRows = await productionOrders.getProductionOrders(ctx.params.plantCode, productionDate);
         ctx.body = JSON.stringify(resultRows);
     } catch (error) {
         console.log(error);
+
     }
 };
 
-const getPanelConfigs = async ctx => {
+/*const setRfidTagInfo = async ctx => {
+    console.log(ctx.request.body);
     try {
-        //console.log("object");
         const config = await panelConfig.getPanelConfig(ctx.params.panelId);
-        ctx.body = JSON.stringify(config);
-        console.log("testyy");
+        const locationCode = config.LocationCode;
+        const rfidType = config.rfidType;
+        const productionorderWithNo = await productionOrders.getProductionOrderWithNo(ctx.params.plantCode, ctx.params.productionNo, ctx.params.productionDate, ctx.params.extend);
+        const {
+            plantCode,
+            rfidNo
+        } = ctx.request.body
+
+        const params = {
+            plantCode: req.plantCode,
+            rfidNo: productionorderWithNo.rfidNo,
+            rfidType: rfidType,
+            prdNo: ctx.params.productionNo,
+            prdLine: productionorderWithNo.,
+            jobId: brandCode: req.brandCode,
+            prdCode: req.prdCode,
+            lotNo: ,
+            shiftCode: req.shiftCode,
+            productionDate: req.productionDate,
+            locationCode: locationCode,
+            userId: req.userId,
+            supervisorCode: req.supervisorCode
+        };
+
+        ctx.body = await rfidInfo.setRfidTagInfo(params);
     } catch (error) {
         console.log(error);
     }
+
 };
 
+*/
+
+
+
 module.exports = {
-    getProductionOrders,
-    getPanelConfigs
+    getProductionOrders //,
+    //setRfidTagInfo
 };
