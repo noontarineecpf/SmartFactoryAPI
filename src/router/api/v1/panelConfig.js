3a7fa140cc5be661291b37fe8db6d6eb0d1bc047
noontarineecpf <49976899+noontarineecpf@users.noreply.github.com>
2019-05-08 13:54:29 +0700

    Update panelConfig.js

--- src/router/api/v1/panelConfig.js
+++ src/router/api/v1/panelConfig.js
@@ -5,7 +5,9 @@ const getPanelConfig = async (panelId) => {
         MAX(DECODE(panel_key,'ProgramCode',panel_Value)) AS ProgramCode,
         MAX(DECODE(panel_key,'LocationCode',panel_Value)) AS LocationCode,
         MAX(DECODE(panel_key,'RfidType',panel_Value)) AS RfidType,
-        MAX(DECODE(panel_key,'StockDocType',panel_Value)) AS StockDocType
+        MAX(DECODE(panel_key,'StockDocType',panel_Value)) AS StockDocType,
+        MAX(DECODE(panel_key,'ModuleCode',panel_Value)) AS ModuleCode,
+        MAX(DECODE(panel_key,'Prefix',panel_Value)) AS Prefix
         from Fd_Panel_Config  where panel_id=:panelId`;
 
         const params = {
@@ -13,7 +15,7 @@ const getPanelConfig = async (panelId) => {
         };
 
         let resultRows = await db.query(sql, params);
-        console.log(resultRows);
+        //console.log(resultRows);
         return resultRows;
     } catch (error) {
         console.log(error);
