2b8763b3e26533f5bef3bec022b89fe40e1938ad
noontarineecpf <49976899+noontarineecpf@users.noreply.github.com>
2019-05-13 14:57:23 +0700

    add log

--- src/router/api/v1/RfidMapRegister.js
+++ src/router/api/v1/RfidMapRegister.js
@@ -3,8 +3,9 @@ const getRfidRegister = async () => {
     try {
         const sql = `SELECT NVL(MAX(REGISTER_NO),0)+1 AS REGISTER_NO 
                     FROM FD_RFID_MAP_REGISTER`;
-        
+
         let resultRows = await db.query(sql);
+        console.log(sql);
         return resultRows;
     } catch (error) {
         console.log(error);
